9f393931c39e1ac7d590d61a837abb8ea9e687f9
Cc <2744166358@qq.com>
2023-09-11 17:17:54 +0800

    feat: 🎸 add app.vue

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+ <div></div>
+</template>
+
+<style>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  border: 1px solid green;
+}
+.vk-tooltip__popper {
+  border: 1px solid red;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
